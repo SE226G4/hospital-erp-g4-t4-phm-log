4f97da5e7f9b6ed787618545851cb8cf66f63dea
Miray-Daher <167767083+Miray-Daher@users.noreply.github.com>
2026-06-05 08:23:43 -0500

    Add files via upload
    sendcost

---
+++ src/MedicationValidator/PharmacyAssignment.Core/SendCostToFinance.cs
@@ -0,0 +1,29 @@
+﻿namespace pharmAssignment.core;
+public class DispensingLog
+{
+    public decimal Cost { get; set; }
+    public bool IsSentToFinance { get; set; }
+
+    public bool SendCostToFinance(bool insuranceProcessed,bool financeSystemAvailable,bool billingItemCreated)
+    {
+        if (!CanSend(insuranceProcessed,billingItemCreated))
+            return false;
+
+        return SubmitToFinance(financeSystemAvailable);
+    }
+    private bool CanSend(bool insuranceProcessed,bool billingItemCreated)
+    {
+        return Cost > 0
+            && !IsSentToFinance
+            && billingItemCreated
+            && insuranceProcessed;
+    }
+    private bool SubmitToFinance(
+        bool financeSystemAvailable)
+    {
+        if (!financeSystemAvailable)
+            return false;
+        IsSentToFinance = true;
+        return true;
+    }
+}
